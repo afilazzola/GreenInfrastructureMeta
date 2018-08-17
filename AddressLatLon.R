48f9c124a2855c5206f92c2bafb114eb56909b69
afilazzola <alex.filazzola@outlook.com>
2018-08-17 16:15:24 -0400

    added data for community gardens in Toronto and surrounding municipalities.

--- AddressLatLon.R
+++ AddressLatLon.R
@@ -38,7 +38,7 @@ write.csv(data, "data//GI.data//GreenRoofGeocoded.csv", row.names=FALSE)
 
 
 
-## Convert lat/lon for allotment and community gardens
+## Convert lat/lon for allotment and community gardens in Toronto
 
 data2 <- read.csv("data//GI.data//GardenData.csv", stringsAsFactors = FALSE)
 
@@ -66,3 +66,54 @@ for(i in missing)
 # Write a CSV file containing origAddress to the working directory
 write.csv(data2, "data//GI.data//GardenGeocoded.csv", row.names=FALSE)
 
+
+
+
+## Convert lat/lon for allotment and community gardens in Peel Region
+
+data3 <- read.csv("data//GI.data//PeelGardenData.csv", stringsAsFactors = FALSE)
+
+
+# Loop through the addresses to get the latitude and longitude of each address and add it to the
+# origAddress data frame in new columns lat and lon
+for(i in 1:nrow(data3))
+{
+  # Print("Working...")
+  result <- geocode(data3$Address[i], output = "latlona", source = "google")
+  data3$lon[i] <- as.numeric(result[1])
+  data3$lat[i] <- as.numeric(result[2])
+}
+
+## Reiterate loop to get all missing NAs as a result of not using Google API (i.e. Over query limit)
+missing <- which(is.na(data3$lat))
+for(i in missing)
+{
+  # Print("Working...")
+  result <- geocode(data2$Address[i], output = "latlona", source = "google")
+  data3$lon[i] <- as.numeric(result[1])
+  data3$lat[i] <- as.numeric(result[2])
+}
+
+# Write a CSV file containing origAddress to the working directory
+write.csv(data3, "data//GI.data//PeelGardenGeocoded.csv", row.names=FALSE)
+
+
+### combine garden datasets
+
+peel <- read.csv("data//GI.data//PeelGardenGeocoded.csv")
+durham <- read.csv("data//GI.data//DurhamGardenGeocoded.csv")
+toronto <- read.csv("data//GI.data//TorontoGardenGeocoded.csv")
+
+peel <- peel[,c("Name","lon","lat")]
+durham <- durham[,c("Name","Lon","Lat")]
+toronto <- toronto[,c("Title","lon","lat")]
+
+colnames(peel) <- c("name","lon","lat")
+colnames(durham) <- c("name","lon","lat")
+colnames(toronto) <- c("name","lon","lat")
+
+gardens <- rbind(peel,durham,toronto)
+gardens[,"region"] <- c(rep("Peel",nrow(peel)),rep("Durham",nrow(durham)),rep("Toronto",nrow(toronto)))
+
+# Write a CSV file containing all community gardens
+write.csv(gardens, "data//GI.data//ComGardensdata.csv", row.names=FALSE)
